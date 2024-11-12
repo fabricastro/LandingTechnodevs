8f387878e4b13e2acd6f2aa737326fbaeedd5485
fabricastro <fabricastrook@gmail.com>
2024-11-11 22:43:02 -0300

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import { motion } from "framer-motion";
+
+import { InvitationModal } from "./InvitationModal";
+import dashboard from "../assets/images/dashboard.jpg";
+
+export const Hero = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  return (
+    <section
+      className="w-screen  flex justify-center items-center bg-bgDark1 mb-[28vw] md:mb-[18vw] lg:mb-[10vw] xl:mb-[13vw] 2xl:mb-60 hero-bg-gradient pb-24 sm:pb-32 md:pb-44 lg:pb-0"
+      id="home"
+    >
+      <div className="w-full md:w-[800px] xl:w-[900px] flex flex-col justify-center items-center pt-16 md:pt-16 lg:pt-20 text-center">
+        
+        <motion.div
+          initial={{ opacity: 0, y: 10, zIndex: 20 }}
+          animate={{ opacity: 1, y: 0, zIndex: 20 }}
+          transition={{ duration: 0.5, delay: 0.15 }}
+        >
+          <div className="relative w-screen flex justify-center ">
+            <img
+              src={dashboard.src}
+              alt="Dashboard image"
+              className="w-4/5 2xl:w-[1200px] mx-auto absolute z-10 rounded-xl main-border-gray hero-dashboard-border-gradient lg:top-6 xl:top-0"
+            />
+          </div>
+        </motion.div>
+        <div className="relative w-screen flex justify-center ">
+          <div className="shape-divider-bottom-1665343298 mt-4 sm:mt-16 md:mt-52 hidden lg:block">
+            <svg
+              data-name="Layer 1"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 1200 120"
+              preserveAspectRatio="none"
+              className="bg-bgDark2"
+            >
+              <path
+                d="M1200 0L0 0 598.97 114.72 1200 0z"
+                className="shape-fill bg-bgDark1  fill-bgDark1"
+              ></path>
+            </svg>
+          </div>
+        </div>
+      </div>
+      {isModalOpen && (
+        <InvitationModal isOpen={isModalOpen} setIsOpen={setIsModalOpen} />
+      )}
+    </section>
+  );
+};
