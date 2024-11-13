6fdda7db54fc9def04a6784afb69981b9b248978
fabricastro <fabricastrook@gmail.com>
2024-11-13 12:55:47 -0300

    subo primera parte de storyblok

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,29 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
+import storyblok from '@storyblok/astro'
+import { loadEnv } from 'vite'
+import tailwind from "@astrojs/tailwind"
+import basicSsl from '@vitejs/plugin-basic-ssl'
 import react from '@astrojs/react';
-import tailwind from "@astrojs/tailwind";
+const env = loadEnv("", process.cwd(), 'STORYBLOK')
 
 export default defineConfig({
-  integrations: [react(), tailwind()]
-});
+  integrations: [storyblok({
+    accessToken: env.STORYBLOK_TOKEN,
+    components: {
+      page: 'storyblok/Page',
+      feature: 'storyblok/Feature',
+      grid: 'storyblok/Grid',
+      teaser: 'storyblok/Teaser',
+      projects: 'storyblok/Projects',
+    },
+    apiOptions: {
+      region: "us",
+    },
+  }), tailwind(), react()],
+  vite: {
+    plugins: [basicSsl()],
+    server: {
+      https: true,
+    },
+  },
+})
