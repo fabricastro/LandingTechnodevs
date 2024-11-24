410c53ffe4327500a7b6111b877cf12c1605ad5f
fabricastro <fabricastrook@gmail.com>
2024-11-24 00:22:22 -0300

    config vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,26 +4,33 @@ import { loadEnv } from 'vite'
 import tailwind from "@astrojs/tailwind"
 import basicSsl from '@vitejs/plugin-basic-ssl'
 import react from '@astrojs/react';
+import vercel from '@astrojs/vercel'; // Añade esta línea
+
 const env = loadEnv("", process.cwd(), 'STORYBLOK')
 
 export default defineConfig({
-  integrations: [storyblok({
-    accessToken: env.STORYBLOK_TOKEN,
-    components: {
-      page: 'storyblok/Page',
-      feature: 'storyblok/Feature',
-      grid: 'storyblok/Grid',
-      teaser: 'storyblok/Teaser',
-      projects: 'storyblok/Projects',
-    },
-    apiOptions: {
-      region: "us",
-    },
-  }), tailwind(), react()],
+  integrations: [
+    storyblok({
+      accessToken: env.STORYBLOK_TOKEN,
+      components: {
+        page: 'storyblok/Page',
+        feature: 'storyblok/Feature',
+        grid: 'storyblok/Grid',
+        teaser: 'storyblok/Teaser',
+        projects: 'storyblok/Projects',
+      },
+      apiOptions: {
+        region: "us",
+      },
+    }),
+    tailwind(),
+    react(),
+  ],
   vite: {
     plugins: [basicSsl()],
     server: {
       https: true,
     },
   },
+  adapter: vercel(),
 })
