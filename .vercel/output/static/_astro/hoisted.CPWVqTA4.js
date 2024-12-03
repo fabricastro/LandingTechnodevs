b5ad39916f9f376fca88cecd0962ff3d30e89fb1
Gabriel <gabrielfunes100202@gmail.com>
2024-12-03 10:42:22 -0300

    build 3/12/24

--- .vercel/output/static/_astro/hoisted.CPWVqTA4.js
+++
@@ -1,1 +0,0 @@
-const n=document.getElementById("hamburgerMenu"),e=document.getElementById("mobileMenu");n.addEventListener("click",()=>{e.classList.toggle("hidden")});e.addEventListener("click",()=>{e.classList.add("hidden")});
