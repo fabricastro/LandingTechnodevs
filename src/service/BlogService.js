0fc3e502cb4258ab5011a9002c32450af27fcb94
Gabriel <gabrielfunes100202@gmail.com>
2025-01-31 15:26:19 -0300

    Agrego vista de blog y merge Server side rendering

--- src/service/BlogService.js
+++ src/service/BlogService.js
@@ -1,7 +1,7 @@
 import PocketBase from 'pocketbase';
 import { getFileUrl } from './utils/getFileUrl';
 import { getAuthorInfo } from './AuthorService';
-import { getCategorieInfo } from './CategorieService';
+import { getAllCategories, getCategorieInfo } from './CategorieService';
 
 const CONTENT_URL = import.meta.env.PUBLIC_CONTENT_URL;
 const COLLECTION_NAME = "posts"
@@ -13,23 +13,24 @@ export const getBlogPosts = async () => {
         sort: '-created',
     });
 
-    const parsedPosts = posts.map( async (post)  =>  ({
+    const parsedPosts = await Promise.all(posts.map(async (post) => ({
         id: post.id,
         title: post.title,
         image: getFileUrl(COLLECTION_NAME, post.id, post.image),
         content: post.content,
         subtitle: post.subtitle,
         date: post.created,
-        categories: await getCategorieInfo(post.categorie),
-    }));
+        categories: await getCategorieInfo(post.categories),
+    })));
+
     return parsedPosts;
 }
+
 
 export const getPostById = async (id) => {
     const post = await pb.collection(COLLECTION_NAME).getOne(id);
     const author = await getAuthorInfo(post.author);
-    const categories = await getCategorieInfo(post.categorie);
-    console.log(categories);
+    const categories = await getCategorieInfo(post.categories);
     const imageUrl = getFileUrl(COLLECTION_NAME, id, post.image);
     return {
         id: post.id,
@@ -48,3 +49,23 @@ export const getCategories = async () => {
     const categories = posts.map((post) => post.categorie);
     return Array.from(new Set(categories));
 }
+
+export const getLatestPostsByCategory = async () => {
+    const categories = await getAllCategories();
+    const categoryPosts = {};
+
+    for (const category of categories) {
+        const posts = await pb.collection(COLLECTION_NAME).getFullList({
+            filter: `categories ~ '${category.id}'`,
+            sort: '-created',
+            perPage: 5,
+        });
+
+        categoryPosts[category.name] = posts.map(post => ({
+            id: post.id,
+            title: post.title,
+            image: getFileUrl(COLLECTION_NAME, post.id, post.image),
+        }));
+    }
+    return categoryPosts;
+};
--- src/service/CategorieService.js
+++ src/service/CategorieService.js
@@ -1,4 +1,5 @@
 import PocketBase from 'pocketbase';
+import { getLatestPostsByCategory } from './BlogService';
 
 const CONTENT_URL = import.meta.env.PUBLIC_CONTENT_URL;
 const COLLECTION_NAME = "post_categories"
@@ -28,3 +29,13 @@ export const getAllCategories = async () => {
     const records = await pb.collection(COLLECTION_NAME).getFullList();
     return records;
 }
+
+export const getCategoriesWithPosts = async () => {
+    const categories = await getAllCategories();
+    const postsByCategory = await getLatestPostsByCategory();
+    return categories.map(category => ({
+        id: category.id,
+        name: category.name,
+        posts: postsByCategory[category.name] || [],
+    }));
+};
