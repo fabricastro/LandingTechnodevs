603b324ea0bf73eee6bba1889b94f3e0021a4c1a
Gabriel <gabrielfunes100202@gmail.com>
2025-02-03 12:30:29 -0300

    Arreglo sitemap

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -5,26 +5,28 @@ export async function GET() {
   const baseUrl = "https://www.technodevs.com.ar";
 
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
-  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    <url>
-      <loc>${baseUrl}</loc>
-      <priority>1.0</priority>
-    </url>
-    ${posts
-        .map(
-          (post) => `
-    <url>
-      <loc>${baseUrl}/blog/${post.id}</loc>
-      <lastmod>${post.date}</lastmod>
-      <priority>0.8</priority>
-    </url>`
-        )
-        .join("")}
-  </urlset>`;
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>${baseUrl}</loc>
+    <priority>1.0</priority>
+  </url>
+  ${posts
+    .map((post) => {
+      const lastmodDate = new Date(post.date).toISOString().split(".")[0] + "+00:00";
+      
+      return `
+  <url>
+    <loc>${baseUrl}/blog/${post.id}</loc>
+    <lastmod>${lastmodDate}</lastmod>
+    <priority>0.8</priority>
+  </url>`;
+    })
+    .join("")}
+</urlset>`;
 
-    return new Response(xml, {
-      headers: {
-        "Content-Type": "application/xml",
-      },
-    });
+  return new Response(xml, {
+    headers: {
+      "Content-Type": "application/xml",
+    },
+  });
 }
